d633296da04fe5d5b25310a00f5a56b593b021a1
wandanuriza <wanda.nuriza.tif20@polban.ac.id>
2021-10-07 23:37:00 +0700

    praktikum 4 selesai

---
+++ Praktikum-4/src/W4/KelasDua.java
@@ -0,0 +1,16 @@
+package W4;
+
+class KelasDua
+{
+	{
+		System.out.println(5);
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(6);
+		KelasSatu satu = new KelasSatu();
+		KelasSatu dua = new KelasSatu(10);
+	}
+}
+
---
+++ Praktikum-4/src/W4/UpinIpin.java
@@ -0,0 +1,8 @@
+package W4;
+
+public class UpinIpin {
+	public static void main(String[] args) {
+		Item name = new Item("upin");
+		}
+	}
+
